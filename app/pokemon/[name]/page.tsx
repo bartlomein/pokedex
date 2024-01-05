88209ff6c6be2f1157a86076892381acf2be9ee7
Bart Tyrpien <bartlomein123@gmail.com>
2024-01-05 15:26:19 -0500

    remove old counterslice

--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -3,12 +3,9 @@
 import { useFetch } from "../../hooks/useFetch";
 
 export default function Name({ params }) {
-  console.log(params.name);
-  console.log("test");
   const { data, error } = useFetch(
     `https://pokeapi.co/api/v2/pokemon-species/${params.name}`
   );
-  console.log(data);
 
   return (
     <>
